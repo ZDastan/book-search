dc267059dfff213c9c3223131482f9aaadafff17
zehra <zehradastan@hotmail.com>
2023-05-23 11:42:39 +0100

    schema created

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
 const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,7 +22,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+};
+// Call the async function to start the server
+startApolloServer();
